c4906d96da51535c9ea5e2a409a27303505319cd
Roland Hauser <holyroli82@gmail.com>
2015-09-22 16:58:24 +0200

    Added pax-cdi testing options

--- src/main/java/ch/sourcepond/testing/bundle/OptionsHelper.java
+++ src/main/java/ch/sourcepond/testing/bundle/OptionsHelper.java
@@ -1,6 +1,9 @@
 package ch.sourcepond.testing.bundle;
 
 import static org.junit.Assert.assertNotNull;
+import static org.ops4j.pax.cdi.test.support.TestConfiguration.cdiProviderBundles;
+import static org.ops4j.pax.cdi.test.support.TestConfiguration.paxCdiProviderAdapter;
+import static org.ops4j.pax.cdi.test.support.TestConfiguration.regressionDefaults;
 import static org.ops4j.pax.exam.CoreOptions.bundle;
 import static org.ops4j.pax.exam.CoreOptions.composite;
 import static org.ops4j.pax.exam.CoreOptions.frameworkProperty;
@@ -24,9 +27,11 @@ public class OptionsHelper {
 	public static Option defaultOptions() {
 		final URL integrationTestJar = OptionsHelper.class.getResource(INTEGRATION_TEST_JAR);
 		assertNotNull(INTEGRATION_TEST_JAR + " could not be found in classpath!", integrationTestJar);
-		return composite(mavenBundle("com.google.guava", "guava").versionAsInProject(),
+		return composite(regressionDefaults(), paxCdiProviderAdapter(), cdiProviderBundles(),
+				mavenBundle("com.google.guava", "guava").versionAsInProject(),
 				mavenBundle("org.apache.commons", "commons-lang3").versionAsInProject(),
-				mavenBundle("org.mockito", "mockito-all").versionAsInProject(), bundle(integrationTestJar.toString()),
-				frameworkProperty("felix.bootdelegation.implicit").value("false"), junitBundles());
+				mavenBundle("org.mockito", "mockito-all").versionAsInProject(),
+				frameworkProperty("felix.bootdelegation.implicit").value("false"),
+				bundle(integrationTestJar.toString()), junitBundles());
 	}
 }
