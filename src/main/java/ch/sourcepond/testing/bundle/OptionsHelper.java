480f48fd9cdf5c48c66bfe9e0df451bc505c0d63
Roland Hauser <holyroli82@gmail.com>
2015-10-09 17:39:51 +0200

    Prepared bundle to be used in separate test projects

--- src/main/java/ch/sourcepond/testing/bundle/OptionsHelper.java
+++ src/main/java/ch/sourcepond/testing/bundle/OptionsHelper.java
@@ -1,16 +1,13 @@
 package ch.sourcepond.testing.bundle;
 
-import static org.junit.Assert.assertNotNull;
 import static org.ops4j.pax.cdi.test.support.TestConfiguration.cdiProviderBundles;
 import static org.ops4j.pax.cdi.test.support.TestConfiguration.paxCdiProviderAdapter;
 import static org.ops4j.pax.cdi.test.support.TestConfiguration.regressionDefaults;
-import static org.ops4j.pax.exam.CoreOptions.bundle;
 import static org.ops4j.pax.exam.CoreOptions.composite;
 import static org.ops4j.pax.exam.CoreOptions.frameworkProperty;
 import static org.ops4j.pax.exam.CoreOptions.junitBundles;
 import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
-
-import java.net.URL;
+import static org.ops4j.pax.exam.CoreOptions.systemProperty;
 
 import org.ops4j.pax.exam.Option;
 
@@ -19,19 +16,19 @@ import org.ops4j.pax.exam.Option;
  *
  */
 public class OptionsHelper {
-	public static final String INTEGRATION_TEST_JAR = "/integrationtest.jar";
 
 	/**
 	 * @return
 	 */
 	public static Option defaultOptions() {
-		final URL integrationTestJar = OptionsHelper.class.getResource(INTEGRATION_TEST_JAR);
-		assertNotNull(INTEGRATION_TEST_JAR + " could not be found in classpath!", integrationTestJar);
 		return composite(regressionDefaults(), paxCdiProviderAdapter(), cdiProviderBundles(),
 				mavenBundle("com.google.guava", "guava").versionAsInProject(),
 				mavenBundle("org.apache.commons", "commons-lang3").versionAsInProject(),
-				mavenBundle("org.mockito", "mockito-all").versionAsInProject(),
-				frameworkProperty("felix.bootdelegation.implicit").value("false"),
-				bundle(integrationTestJar.toString()), junitBundles());
+				mavenBundle("org.mockito", "mockito-core").versionAsInProject(),
+				frameworkProperty("felix.bootdelegation.implicit").value("false"), junitBundles(),
+
+				// Override regression default; we use the logback.xml located
+				// in this jar
+				systemProperty("logback.configurationFile").value(""));
 	}
 }
