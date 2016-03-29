a0d914c1a2179c2282540cf612b838f357511e79
Roland Hauser <sourcepond@gmail.com>
2016-03-29 15:15:30 +0200

    Refactored project

--- test-support-osgi/src/main/java/ch/sourcepond/testing/OptionsHelper.java
+++ test-support-osgi/src/main/java/ch/sourcepond/testing/OptionsHelper.java
@@ -1,8 +1,18 @@
-package ch.sourcepond.testing.bundle;
+/*Copyright (C) 2015 Roland Hauser, <sourcepond@gmail.com>
 
-import static org.ops4j.pax.cdi.test.support.TestConfiguration.cdiProviderBundles;
-import static org.ops4j.pax.cdi.test.support.TestConfiguration.paxCdiProviderAdapter;
-import static org.ops4j.pax.cdi.test.support.TestConfiguration.regressionDefaults;
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.*/
+package ch.sourcepond.testing;
+
 import static org.ops4j.pax.exam.CoreOptions.composite;
 import static org.ops4j.pax.exam.CoreOptions.frameworkProperty;
 import static org.ops4j.pax.exam.CoreOptions.junitBundles;
@@ -14,7 +24,7 @@ import static org.ops4j.pax.exam.CoreOptions.wrappedBundle;
 import org.ops4j.pax.exam.Option;
 
 /**
- * @author rolandhauser
+ * Small helper class for Pax-Exam test configuration.
  *
  */
 public class OptionsHelper {
@@ -36,15 +46,15 @@ public class OptionsHelper {
 	 * @return Option to be passed to the Pax-Exam test-container.
 	 */
 	public static Option defaultOptions(final String pExaminee, final String... pDependencyComponent) {
-		return composite(regressionDefaults(), paxCdiProviderAdapter(), cdiProviderBundles(), examineeOption(pExaminee),
-				dependenciesOption(pDependencyComponent), mavenBundle("com.google.guava", "guava").versionAsInProject(),
+		return composite(examineeOption(pExaminee), dependenciesOption(pDependencyComponent),
+				mavenBundle("com.google.guava", "guava").versionAsInProject(),
 				mavenBundle("org.apache.commons", "commons-lang3").versionAsInProject(),
 				mavenBundle("org.objenesis", "objenesis").versionAsInProject(),
 				mavenBundle("org.mockito", "mockito-core").versionAsInProject(),
 				frameworkProperty("felix.bootdelegation.implicit").value("false"), junitBundles(),
 
-				// Override regression default; we use the logback.xml located
-				// in this jar
+		// Override regression default; we use the logback.xml located
+		// in this jar
 				systemProperty("logback.configurationFile").value(""));
 	}
 
